434f830d8a5e7a418b11f1201f859730ac97a387
Anna Schmidt <marshall1697@gmail.com>
2022-11-01 10:53:30 -0600

    commit tree e6f53e1c4c2fbfc861fe1db820d5d2298286cd47

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :plants, only: [:index, :show, :create]
+
+  # get '/plants', to: 'plants#inded'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
